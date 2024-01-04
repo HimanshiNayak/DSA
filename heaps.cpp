0cf664eb67eb33149e9111d565e7d59623abbacb
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2024-01-04 16:52:13 +0530

    heaps.cpp

--- heaps.cpp
+++ heaps.cpp
@@ -23,6 +23,31 @@ class heap{
 		}
 	}
 	}
+        void deleteroot(){
+		if(n==0){
+			cout<<"Nothing is there to be deleted.";
+			return;
+		}
+	a[1]=a[n];
+	n--;
+	//take root to right position
+	int i=1;
+	while(i<=n){ // because size was decreased by one
+	int left = 2*i;
+	int right=2*i+1;
+	if(left<=n && a[i]<a[left]){
+		swap(a[left],a[i]);
+		i=left;
+	}	
+	else if(right<=n && a[i]<a[right]){
+		swap(a[right],a[i]);
+		i=right;
+	}
+	else{
+		return;
+	}
+	}
+	}
 	void print(){
 		for(int i=1;i<=n;i++){
 			cout<<a[i]<<" ";
@@ -37,5 +62,7 @@ int main(){
 	h.insert(53);
 	h.insert(52);
 	h.insert(54);
+	h.print();
+	h.deleteroot();
 	h.print();
 }
